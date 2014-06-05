e4ff65772dbcd194647d800de0a8f5f5b5089526
KittyMac <rocco@chimerasw.com>
2014-06-05 16:35:28 -0400

    Do not adjust non PlanetUnity shader names

--- PUGameObject.cs
+++ PUGameObject.cs
@@ -42,6 +42,10 @@ public partial class PUGameObject : PUGameObjectBase {
 		if (shaderPath.StartsWith ("PlanetUnity/DepthMask")) {
 			return shaderPath;
 		}
+
+		if (shaderPath.StartsWith ("PlanetUnity/") == false) {
+			return shaderPath;
+		}
 		
 		// Check to see if I am inside of something that is clipping...
 		if (depthMaskCounter > 0) {
